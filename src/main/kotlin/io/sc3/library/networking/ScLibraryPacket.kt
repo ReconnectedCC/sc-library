ef72d8624c8a0198ec1d8414fbb0f45e93b474a9
yourfriendoss <sofija@duck.com>
2025-07-08 15:11:38 +0300

    Get rid of NetworkUtil (nothing uses it), completely recode ScLibraryPacket

--- src/main/kotlin/io/sc3/library/networking/ScLibraryPacket.kt
+++ src/main/kotlin/io/sc3/library/networking/ScLibraryPacket.kt
@@ -1,22 +1,19 @@
 package io.sc3.library.networking
 
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking.createC2SPacket
 import net.fabricmc.fabric.api.event.Event
 import net.fabricmc.fabric.api.event.EventFactory
-import net.fabricmc.fabric.api.networking.v1.PacketByteBufs
 import net.fabricmc.fabric.api.networking.v1.PacketSender
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking
-import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking.createS2CPacket
-import net.minecraft.client.MinecraftClient
-import net.minecraft.client.network.ClientPlayNetworkHandler
-import net.minecraft.network.PacketByteBuf
+import net.minecraft.entity.EntityType
+import net.minecraft.entity.SpawnReason
+import net.minecraft.network.RegistryByteBuf
+import net.minecraft.network.codec.PacketCodec
 import net.minecraft.network.packet.CustomPayload
-import net.minecraft.network.packet.Packet
-import net.minecraft.server.MinecraftServer
 import net.minecraft.server.network.ServerPlayNetworkHandler
 import net.minecraft.server.network.ServerPlayerEntity
-import net.minecraft.util.Identifier
+import net.minecraft.util.math.BlockPos
+
 
 inline fun <reified T> event(noinline invokerFactory: (Array<T>) -> T): Event<T>
   = EventFactory.createArrayBacked(T::class.java, invokerFactory)
@@ -31,29 +28,23 @@ inline fun <reified T> serverPacketEvent() = event<(packet: T, player: ServerPla
   { packet, player, handler, responseSender -> cb.forEach { it(packet, player, handler, responseSender) } }
 }
 
-fun <T: ScLibraryPacket> registerClientReceiver(id: CustomPayload.Id<CustomPayload>, factory: (buf: CustomPayload) -> T) {
-  ClientPlayNetworking.registerGlobalReceiver(id) { payload, ctx ->
-    val packet = factory(payload)
-    packet.onClientReceive(payload, ctx)
-  }
-}
-
-fun <T: ScLibraryPacket> registerServerReceiver(id: CustomPayload.Id<CustomPayload>, factory: (buf: CustomPayload) -> T) {
+fun <T : ScLibraryPacket> registerServerReceiver(
+  id: CustomPayload.Id<T>
+) {
   ServerPlayNetworking.registerGlobalReceiver(id) { payload, ctx ->
-    val packet = factory(payload)
-    packet.onServerReceive(payload, ctx)
+    payload.onServerReceive(ctx)
   }
 }
 
-abstract class ScLibraryPacket {
-  abstract val id: CustomPayload.Id<CustomPayload>
+fun <T : ScLibraryPacket> registerClientReceiver(
+  id: CustomPayload.Id<T>
+) {
+  ClientPlayNetworking.registerGlobalReceiver(id) { payload, ctx ->
+    payload.onClientReceive(ctx)
+  }
+}
 
-  abstract val payload: CustomPayload;
-  
-  fun toC2SPacket(): Packet<*> = createC2SPacket(payload)
-  fun toS2CPacket(): Packet<*> = createS2CPacket(payload)
-
-  open fun onClientReceive(payload: CustomPayload, ctx: ClientPlayNetworking.Context) {}
-
-  open fun onServerReceive(payload: CustomPayload, ctx: ServerPlayNetworking.Context) {}
+abstract class ScLibraryPacket : CustomPayload {
+  abstract fun onServerReceive(ctx: ServerPlayNetworking.Context)
+  abstract fun onClientReceive(ctx: ClientPlayNetworking.Context)
 }
