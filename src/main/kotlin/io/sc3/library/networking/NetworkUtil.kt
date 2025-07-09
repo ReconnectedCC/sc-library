6aae144b7fdd7c30d9d8370522afaf9feaf07364
yourfriendoss <sofija@duck.com>
2025-07-09 22:41:12 +0300

    Revert: "Get rid of NetworkUtil (nothing uses it)"

---
+++ src/main/kotlin/io/sc3/library/networking/NetworkUtil.kt
@@ -0,0 +1,14 @@
+package io.sc3.library.networking
+
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking
+import net.minecraft.server.world.ServerChunkManager
+import net.minecraft.world.chunk.WorldChunk
+
+object NetworkUtil {
+  fun sendToAllTracking(chunk: WorldChunk, packet: ScLibraryPacket) {
+    val storage = (chunk.world.chunkManager as ServerChunkManager).chunkLoadingManager
+    storage.getPlayersWatchingChunk(chunk.pos, false).forEach { a ->
+      ServerPlayNetworking.send(a, packet);
+    }
+  }
+}
